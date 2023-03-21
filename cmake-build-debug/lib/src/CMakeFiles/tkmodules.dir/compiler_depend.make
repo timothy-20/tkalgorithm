1f52cd818d9475662ad47e664eb9f6509b0528ea
timothy-20 <jungun3329@gmail.com>
2023-03-21 18:41:37 +0900

    230321) - .gitignore 적용.

--- cmake-build-debug/lib/src/CMakeFiles/tkmodules.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for tkmodules.
-# This may be replaced when dependencies are built.
--- cmake-build-wsl_profile/lib/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Windows MFC Projects/tkalgorithm/lib
-# Build directory: /mnt/d/Windows MFC Projects/tkalgorithm/cmake-build-wsl_profile/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test]=] "/mnt/d/Windows MFC Projects/tkalgorithm/cmake-build-wsl_profile/lib/test")
-set_tests_properties([=[test]=] PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/Windows MFC Projects/tkalgorithm/lib/CMakeLists.txt;26;add_test;/mnt/d/Windows MFC Projects/tkalgorithm/lib/CMakeLists.txt;0;")
