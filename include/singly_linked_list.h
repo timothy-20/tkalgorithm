b66b3060657cc8b00a0066e314409f50fc1f51eb
timothy-20 <jungun3329@gmail.com>
2023-03-23 22:53:08 +0900

    230323) - singly linked list, iterator 구현 및 적용 중.

--- include/singly_linked_list.h
+++ include/singly_linked_list.h
@@ -9,47 +9,77 @@
 namespace tk {
     template<typename t>
     class singly_linked_list {
+    public:
+        template <typename it>
+        class iterator {
+        public:
+            using value_type = t;
+            using pointer_type = t*;
+            using reference_type = t&;
+            using difference_type = std::ptrdiff_t;
+            using iterator_category = std::forward_iterator_tag;
+
+            explicit iterator(pointer_type value) : _pointer_value(value) {}
+            reference_type operator*() { return *this->_pointer_value; }
+            pointer_type operator->() { return this->_pointer_value; }
+            iterator<it>& operator++() {
+                ++this->_pointer_value;
+
+                return *this;
+            }
+            iterator<it> operator++(it) {
+                iterator<it> temp(*this);
+                this->_pointer_value++;
+
+                return temp;
+            }
+            bool operator==(const iterator<it>& other) { return this->_pointer_value == other._pointer_value; }
+            bool operator!=(const iterator<it>& other) { return this->_pointer_value != other._pointer_value; }
+
+        private:
+            pointer_type _pointer_value;
+        };
+
+    public:
+        explicit singly_linked_list() :
+        _front(nullptr),
+        _count(0) {}
+        ~singly_linked_list() = default;
+        void add_front(const t& value) {
+            auto new_node(std::make_unique<node<t>>(value));
+            new_node->_next = std::move(this->_front);
+            this->_front = std::move(new_node);
+            this->_count++;
+        }
+        void remove_front() {
+            if (this->_front) {
+                this->_front = this->_front->_next;
+                this->_count--;
+            }
+        }
+        void insert_after(uint32_t index) {
+
+        }
+        void remove_after(uint32_t index) {
+
+        }
+        t front() const { return this->_front->_value; }
+        size_t count() const { return this->_count; }
+
     private:
         template <typename nt>
         struct node {
-            t _value;
-            node<t>* _next;
+            nt _value;
+            std::unique_ptr<node<nt>> _next;
 
-            explicit node(t value) :
-            _value(value),
-            _next(nullptr) {}
-            node() : node(t()) {}
-            ~node() { delete this->_next; }
+            explicit node(const nt& value) :
+                    _value(value),
+                    _next(nullptr) {}
+            node() : node(nt()) {}
         };
 
     private:
         std::unique_ptr<node<t>> _front;
         size_t _count;
-
-    public:
-        explicit singly_linked_list(size_t count = 1) :
-        _front(std::make_unique<node<t>>()),
-        _count(count) {
-        }
-        void add_front(t value) {
-            auto new_node(new node<t>(value));
-            new_node->_next = this->_front;
-            this->_front = new_node;
-            this->_count++;
-        }
-        void remove_front() {
-            auto next_node(this->_front->_next);
-            this->_front->_next = nullptr;
-            delete this->_front;
-            this->_front = next_node;
-            this->_count++;
-        }
-        void insert_after() {
-
-        }
-        void remove_after() {
-
-        }
-        t front() const { return this->_front->_value; }
     };
 }
--- test/singly_linked_list_test.cpp
+++ test/singly_linked_list_test.cpp
@@ -9,4 +9,16 @@ TEST(test_suite_singly_linked_list, initialize_test) {
     tk::singly_linked_list<int> list;
 
     EXPECT_EQ(list.front(), 0);
+    EXPECT_EQ(list.count(), 1);
+}
+
+TEST(test_suite_singly_linked_list, add_front_test) {
+    tk::singly_linked_list<int> list;
+
+    for (int i(0); i < 3; i++) {
+        list.add_front(i);
+    }
+
+    EXPECT_EQ(list.count(), 3);
+    EXPECT_EQ(list.front(), 2);
 }
