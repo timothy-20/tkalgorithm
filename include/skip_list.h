6e7ede00aded6140456951cf9af21ecc00e061f8
timothy <jungun3329@gmail.com>
2023-03-31 15:02:31 +0900

    230331) - skip_list.h, insert 구현 중.

--- include/skip_list.h
+++ include/skip_list.h
@@ -16,7 +16,9 @@ namespace tk {
             explicit node(t value, size_t level) :
             _value(value),
             _forward(new node*[level+1]) {
-                memset(this->_forward, t(), sizeof(node*) * (level+1));
+                if (memset(this->_forward, nullptr, sizeof(node*) * (level+1)) == nullptr) {
+                    throw std::bad_alloc(); // forward 초기화 실패 시, 예외 전이
+                }
             }
             ~node() {
                 delete[] this->_forward;
@@ -24,33 +26,53 @@ namespace tk {
         };
 
     private:
-        static int constexpr MAX_LEVEL = 10;
-        size_t _level;
+        static int constexpr MAX_LEVEL = 10; // 한 노드가 가질 수 있는 최대 레벨
+        size_t _count;
         node* _head;
 
     public:
-        skip_list() :
-        _level(1),
+        skip_list() try :
+        _count(1), // head가 존재하므로 1로 초기화
         _head(new node(t(), MAX_LEVEL)) {
             srand((unsigned int)time(nullptr));
+
+        } catch (std::exception e) { // function try
+            delete this->_head;
+            throw e;
         }
         ~skip_list() {
             delete this->_head;
         }
 
-        void insert_node(t const& value) {
+        int random_level() const {
+            int level(1);
+
+            while (rand() % 2 == 0 && level < MAX_LEVEL) {
+                level++;
+            }
+
+            return level;
+        }
+
+        void insert(t const& value) {
             node* temp(this->_head);
             node* update_node[MAX_LEVEL+1];
 
-            memset(update_node, t(), sizeof(node*) * (MAX_LEVEL+1));
+            memset(update_node, nullptr, sizeof(node*) * (MAX_LEVEL+1));
 
-            for (int i(this->_level); i > 0; i--) {
+            for (int i(this->_count); i > 0; i--) {
                 while (temp->_forward[i] && temp->_forward[i]->_value < value) {
-
+                    temp = temp->_forward[i];
                 }
 
                 update_node[i] = temp;
             }
+
+            if (temp->_forward[1] && temp->_forward[1]->_value == value) {
+                return;
+            }
+
+
         }
     };
 }
