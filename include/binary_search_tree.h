8c6ab36475e336489eb5aeb138bc783a22368c7c
timothy-20 <jungun3329@gmail.com>
2023-04-11 22:47:24 +0900

    230411) - binary_search_tree.h, 이진 탐색 트리 기본 동작 인터페이스로 분리. - binary_search_tree.h, 배열 기반의 이진 탐색 트리 구현 중.

--- include/binary_search_tree.h
+++ include/binary_search_tree.h
@@ -9,36 +9,77 @@
 namespace tk {
     template <typename t>
     class binary_search_tree {
-    private:
-        struct node {
-            t _value;
-            node* _left;
-            node* _right;
+    public:
+        virtual void* search(t const& value) = 0;
+        virtual void* traversal_preorder(void* root, std::function<void(void* target)> completion) const = 0;
+        virtual void* traversal_inorder(void* root, std::function<void(void* target)> completion) const = 0;
+        virtual void* traversal_postorder(void* root, std::function<void(void* target)> completion) const = 0;
+        virtual void insert(t const& value) = 0;
+        virtual void remove(t const& value) = 0;
+    };
 
-            explicit node(t const& value) :
-            _value(value),
-            _left(nullptr),
-            _right(nullptr) {}
-            node() : node(t()) {}
-        };
-
+    template <typename t>
+    class array_based_bst : public binary_search_tree<t> {
     private:
         size_t _size;
-        node* _root;
+        t* _tree;
 
     private:
-        void search(t const& value, std::function<void(node* parent, node* current, bool is_left)> completion);
-        void traversal_preorder(node* root, std::function<void(node* target)> completion);
-        void traversal_inorder(node* root, std::function<void(node* target)> completion);
-        void traversal_postorder(node* root, std::function<void(node* target)> completion);
+        size_t get_parent_index(size_t index) const {
+            return (index - (index % 2 == 1 ? 1 : 2)) / 2;
+        }
+        size_t get_child_index(size_t index, bool is_left) const {
+            return 2 * index + (is_left ? 1 : 2);
+        }
 
     public:
-        binary_search_tree();
-        binary_search_tree(std::initializer_list<t> list);
-        explicit binary_search_tree(size_t size, t const& value);
-        explicit binary_search_tree(size_t size);
-        ~binary_search_tree();
-        void insert(t const& value);
-        void remove(t const& value);
+        explicit array_based_bst(size_t size, t const& value) :
+        _size(size),
+        _tree(new t[size+1]) {
+            for (int i(0); i < size; i++) {
+                this->_tree[i] = value;
+            }
+        }
+        ~array_based_bst() {
+            delete[] this->_tree;
+        }
+        void insert(t const& value) override {
+
+        }
+    };
+
+    template <typename t>
+    class linked_list_based_bst : binary_search_tree<t> {
+//    private:
+//        struct node {
+//            t _value;
+//            node* _left;
+//            node* _right;
+//
+//            explicit node(t const& value) :
+//                    _value(value),
+//                    _left(nullptr),
+//                    _right(nullptr) {}
+//            node() : node(t()) {}
+//        };
+//
+//    private:
+//        size_t _size;
+//        node* _root;
+//
+//    private:
+//        void search(t const& value, std::function<void(node* parent, node* current, bool is_left)> completion);
+//        void traversal_preorder(node* root, std::function<void(node* target)> completion);
+//        void traversal_inorder(node* root, std::function<void(node* target)> completion);
+//        void traversal_postorder(node* root, std::function<void(node* target)> completion);
+//
+//    public:
+//        binary_search_tree();
+//        binary_search_tree(std::initializer_list<t> list);
+//        explicit binary_search_tree(size_t size, t const& value);
+//        explicit binary_search_tree(size_t size);
+//        ~binary_search_tree();
+//        void insert(t const& value);
+//        void remove(t const& value);
     };
 }
--- src/binary_search_tree.cpp
+++ src/binary_search_tree.cpp
@@ -5,163 +5,163 @@
 #include <binary_search_tree.h>
 
 namespace tk {
-    template <typename t>
-    binary_search_tree<t>::binary_search_tree():
-    _root(nullptr),
-    _size(0) {}
-
-    template <typename t>
-    binary_search_tree<t>::binary_search_tree(std::initializer_list<t> list) : binary_search_tree() {
-        for (auto const& value : list) {
-            this->insert(value);
-        }
-    }
-
-    template <typename t>
-    binary_search_tree<t>::binary_search_tree(size_t size, t const& value) : binary_search_tree() {
-        for (int i(0); i < size; i++) {
-            this->insert(value);
-        }
-    }
-
-    template <typename t>
-    binary_search_tree<t>::binary_search_tree(size_t size) : binary_search_tree(size, t()) {}
-
-    template <typename t>
-    binary_search_tree<t>::~binary_search_tree() {
-        this->traversal_postorder(this->_root, [](node* target) {
-            delete target;
-        });
-    }
-
-    template <typename t>
-    void binary_search_tree<t>::search(t const& value, std::function<void(node*, node*, bool)> completion) {
-        node* parent;
-        auto current(this->_root);
-        bool is_left(false);
-
-        while (current && current->_value != value) {
-            parent = current;
-
-            if (value < current->_value) {
-                is_left = true;
-                current = current->_left;
-
-            } else if (value > current->_value) {
-                is_left = false;
-                current = current->_right;
-            }
-        }
-
-        completion(parent, current, is_left);
-    }
-
-    template <typename t>
-    void binary_search_tree<t>::traversal_preorder(typename binary_search_tree<t>::node* root, std::function<void(node*)> completion) {
-        if (root == nullptr) {
-            return;
-        }
-
-        completion(root);
-        this->traversal_preorder(root->_left, completion);
-        this->traversal_preorder(root->_right, completion);
-    }
-
-    template <typename t>
-    void binary_search_tree<t>::traversal_inorder(typename binary_search_tree<t>::node* root, std::function<void(node*)> completion) {
-        if (root == nullptr) {
-            return;
-        }
-
-        this->traversal_inorder(root->_left, completion);
-        completion(root);
-        this->traversal_inorder(root->_right, completion);
-    }
-
-    template <typename t>
-    void binary_search_tree<t>::traversal_postorder(typename binary_search_tree<t>::node* root, std::function<void(node*)> completion) {
-        if (root == nullptr) {
-            return;
-        }
-
-        this->traversal_postorder(root->_left, completion);
-        this->traversal_postorder(root->_right, completion);
-        completion(root);
-    }
-
-    template <typename t>
-    void binary_search_tree<t>::insert(t const& value) {
-        auto new_node(new node(value));
-
-        if (this->_root == nullptr) {
-            this->_root = new_node;
-
-            return;
-        }
-
-        this->search(value, [this, &new_node](node* parent, node* current, bool is_left) {
-            if (parent && current == nullptr) { // leaf 노드에 도달한 경우
-                if (is_left) {
-                    parent->_left = new_node;
-
-                } else {
-                    parent->_right = new_node;
-                }
-
-                ++this->_size;
-
-            } else { // 동일한 값을 가진 노드를 발견한 경우
-                delete new_node;
-            }
-        });
-    }
-
-    template <typename t>
-    void binary_search_tree<t>::remove(t const& value) {
-        if (this->_root == nullptr) {
-            return;
-        }
-
-        this->search(value, [this](node* parent, node* current, bool is_left) {
-            if (parent && current) {
-                node* replace_node;
-
-                if (current->_left == nullptr || current->_right == nullptr) { // 대상 노드의 자식이 1개만 있는 경우
-                    replace_node = current->_left ? : current->_right;
-
-                } else if (current->_left && current->_right) { // 대상 노드의 자식 노드가 둘 다 있는 경우
-                    auto successor_parent(current);
-                    auto successor(current->_right);
-
-                    while (successor->_left) { // 오른쪽 노드의 자식 중에 최소 값을 가진 노드 탐색(왼쪽 자식 노드가 없는 노드)
-                        successor_parent = successor;
-                        successor = successor->_left;
-                    }
-
-                    if (current->_right == successor) { // 최소 노드가 인접해 있는 경우
-                        successor->_left = current->_left;
-
-                    } else {
-                        successor_parent->_left = successor->_right; // 최소 노드에 오른쪽 자식 노드가 있는 경우 부모 노드와 연결, 아니면 nullptr 대입
-                        successor->_left = current->_left;
-                        successor->_right = current->_right;
-                    }
-
-                    replace_node = successor;
-                }
-
-                if (is_left) {
-                    parent->_left = replace_node;
-
-                } else {
-                    parent->_right = replace_node;
-                }
-
-                delete current;
-                --this->_size;
-            }
-        });
-    }
-
-    template class binary_search_tree<int>;
+//    template <typename t>
+//    binary_search_tree<t>::binary_search_tree():
+//    _root(nullptr),
+//    _size(0) {}
+//
+//    template <typename t>
+//    binary_search_tree<t>::binary_search_tree(std::initializer_list<t> list) : binary_search_tree() {
+//        for (auto const& value : list) {
+//            this->insert(value);
+//        }
+//    }
+//
+//    template <typename t>
+//    binary_search_tree<t>::binary_search_tree(size_t size, t const& value) : binary_search_tree() {
+//        for (int i(0); i < size; i++) {
+//            this->insert(value);
+//        }
+//    }
+//
+//    template <typename t>
+//    binary_search_tree<t>::binary_search_tree(size_t size) : binary_search_tree(size, t()) {}
+//
+//    template <typename t>
+//    binary_search_tree<t>::~binary_search_tree() {
+//        this->traversal_postorder(this->_root, [](node* target) {
+//            delete target;
+//        });
+//    }
+//
+//    template <typename t>
+//    void binary_search_tree<t>::search(t const& value, std::function<void(node*, node*, bool)> completion) {
+//        node* parent;
+//        auto current(this->_root);
+//        bool is_left(false);
+//
+//        while (current && current->_value != value) {
+//            parent = current;
+//
+//            if (value < current->_value) {
+//                is_left = true;
+//                current = current->_left;
+//
+//            } else if (value > current->_value) {
+//                is_left = false;
+//                current = current->_right;
+//            }
+//        }
+//
+//        completion(parent, current, is_left);
+//    }
+//
+//    template <typename t>
+//    void binary_search_tree<t>::traversal_preorder(typename binary_search_tree<t>::node* root, std::function<void(node*)> completion) {
+//        if (root == nullptr) {
+//            return;
+//        }
+//
+//        completion(root);
+//        this->traversal_preorder(root->_left, completion);
+//        this->traversal_preorder(root->_right, completion);
+//    }
+//
+//    template <typename t>
+//    void binary_search_tree<t>::traversal_inorder(typename binary_search_tree<t>::node* root, std::function<void(node*)> completion) {
+//        if (root == nullptr) {
+//            return;
+//        }
+//
+//        this->traversal_inorder(root->_left, completion);
+//        completion(root);
+//        this->traversal_inorder(root->_right, completion);
+//    }
+//
+//    template <typename t>
+//    void binary_search_tree<t>::traversal_postorder(typename binary_search_tree<t>::node* root, std::function<void(node*)> completion) {
+//        if (root == nullptr) {
+//            return;
+//        }
+//
+//        this->traversal_postorder(root->_left, completion);
+//        this->traversal_postorder(root->_right, completion);
+//        completion(root);
+//    }
+//
+//    template <typename t>
+//    void binary_search_tree<t>::insert(t const& value) {
+//        auto new_node(new node(value));
+//
+//        if (this->_root == nullptr) {
+//            this->_root = new_node;
+//
+//            return;
+//        }
+//
+//        this->search(value, [this, &new_node](node* parent, node* current, bool is_left) {
+//            if (parent && current == nullptr) { // leaf 노드에 도달한 경우
+//                if (is_left) {
+//                    parent->_left = new_node;
+//
+//                } else {
+//                    parent->_right = new_node;
+//                }
+//
+//                ++this->_size;
+//
+//            } else { // 동일한 값을 가진 노드를 발견한 경우
+//                delete new_node;
+//            }
+//        });
+//    }
+//
+//    template <typename t>
+//    void binary_search_tree<t>::remove(t const& value) {
+//        if (this->_root == nullptr) {
+//            return;
+//        }
+//
+//        this->search(value, [this](node* parent, node* current, bool is_left) {
+//            if (parent && current) {
+//                node* replace_node;
+//
+//                if (current->_left == nullptr || current->_right == nullptr) { // 대상 노드의 자식이 1개만 있는 경우
+//                    replace_node = current->_left ? : current->_right;
+//
+//                } else if (current->_left && current->_right) { // 대상 노드의 자식 노드가 둘 다 있는 경우
+//                    auto successor_parent(current);
+//                    auto successor(current->_right);
+//
+//                    while (successor->_left) { // 오른쪽 노드의 자식 중에 최소 값을 가진 노드 탐색(왼쪽 자식 노드가 없는 노드)
+//                        successor_parent = successor;
+//                        successor = successor->_left;
+//                    }
+//
+//                    if (current->_right == successor) { // 최소 노드가 인접해 있는 경우
+//                        successor->_left = current->_left;
+//
+//                    } else {
+//                        successor_parent->_left = successor->_right; // 최소 노드에 오른쪽 자식 노드가 있는 경우 부모 노드와 연결, 아니면 nullptr 대입
+//                        successor->_left = current->_left;
+//                        successor->_right = current->_right;
+//                    }
+//
+//                    replace_node = successor;
+//                }
+//
+//                if (is_left) {
+//                    parent->_left = replace_node;
+//
+//                } else {
+//                    parent->_right = replace_node;
+//                }
+//
+//                delete current;
+//                --this->_size;
+//            }
+//        });
+//    }
+//
+//    template class binary_search_tree<int>;
 }
