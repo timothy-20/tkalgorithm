61f72b5900780a9a214ffd48879ea63910b247ad
timothy <jungun3329@gmail.com>
2023-04-14 16:14:04 +0900

    230414) - binary_search_tree.cpp, 소스 파일에 구현 옮김. - array_based_bst, 추가적인 유틸 함수 구현. - array_based_bst, 삭제 기능 구현 중.

--- include/binary_search_tree.h
+++ include/binary_search_tree.h
@@ -12,6 +12,7 @@ namespace tk {
     class binary_search_tree { // 이진 탐색 트리 인터페이스
     public:
         virtual void search(value_t const& value, std::function<void(node_t parent, node_t current, bool is_left)> const& completion) const = 0;
+        virtual node_t search_edge(node_t root, bool is_min) const = 0;
         virtual void traversal_preorder(node_t root, std::function<void(value_t& value)> const& completion) const = 0;
         virtual void traversal_inorder(node_t root, std::function<void(value_t& value)> const& completion) const = 0;
         virtual void traversal_postorder(node_t root, std::function<void(value_t& value)> const& completion) const = 0;
@@ -26,164 +27,21 @@ namespace tk {
         t* _tree;
 
     private:
-        size_t get_parent_index(size_t index) const {
-            return (index - (index % 2 == 1 ? 1 : 2)) / 2;
-        }
-        size_t get_child_index(size_t index, bool is_left) const {
-            return 2 * index + (is_left ? 1 : 2);
-        }
-        void resize(size_t new_size) {
-            if (this->_tree == nullptr) {
-                return;
-            }
-
-            auto new_tree(new t[new_size]);
-            auto end(std::copy(this->_tree, this->_tree + (new_size >= this->_size ? this->_size : new_size), new_tree));
-            std::fill(end, new_tree + new_size, t());
-
-            delete[] this->_tree;
-
-            this->_tree = new_tree;
-            this->_size = new_size;
-        }
+        size_t get_parent_index(size_t index) const;
+        size_t get_child_index(size_t index, bool is_left) const;
+        bool is_exist(size_t index) const;
+        void resize(size_t new_size);
 
     public:
-        explicit array_based_bst(size_t size = 14) : // level 3 까지는 기본적으로 할당
-        _size(size),
-        _tree(new t[size]) {
-            std::fill(this->_tree, this->_tree + this->_size, t());
-        }
-        ~array_based_bst() {
-            delete[] this->_tree;
-        }
-        void search(t const& value, std::function<void(size_t parent, size_t current, bool is_left)> const& completion) const override {
-            if (this->_tree[0] == t()) {
-                return;
-            }
-
-            size_t parent_index(0);
-            size_t current_index(0);
-            bool is_left(false);
-
-            while (current_index < this->_size) { // 트리의 크기를 넘지 않을 때까지 순회
-                if (this->_tree[current_index] == value) {
-                    break;
-                }
-
-                parent_index = current_index;
-                is_left = this->_tree[parent_index] > value;
-                current_index = this->get_child_index(parent_index, is_left);
-            }
-
-            completion(parent_index, current_index, is_left);
-        }
-        void traversal_preorder(size_t root, std::function<void(t& value)> const& completion) const override {
-            auto index_stack(new size_t[this->_size]);
-            long index_stack_cursor(0);
-            index_stack[index_stack_cursor] = 0;
-
-            while (index_stack_cursor >= 0) {
-                auto current_index(index_stack[index_stack_cursor--]);
-
-                if (current_index >= this->_size || this->_tree[current_index] == t()) {
-                    continue;
-                }
-
-                completion(this->_tree[current_index]);
-
-                auto right_child_index(this->get_child_index(current_index, false));
-                auto left_child_index(this->get_child_index(current_index, true));
-
-                if (right_child_index < this->_size) {
-                    index_stack[++index_stack_cursor] = right_child_index;
-                }
-
-                if (left_child_index < this->_size) {
-                    index_stack[++index_stack_cursor] = left_child_index;
-                }
-            }
-
-            delete[] index_stack;
-        }
-        void traversal_inorder(size_t root, std::function<void(t& value)> const& completion) const override {
-            auto index_stack(new size_t[this->_size]);
-            long index_stack_cursor(-1);
-            size_t current_index(0);
-
-            while (index_stack_cursor >= 0 || current_index < this->_size) {
-                if (current_index < this->_size && this->_tree[current_index] != t()) { // 현재 인덱스가 가르키고 있는 대상 유효할 경우(우선적으로 왼쪽 노드로 이동하기 위한 조건)
-                    index_stack[++index_stack_cursor] = current_index;
-                    current_index = this->get_child_index(current_index, true); // 왼쪽 자식의 인덱스 이동
-
-                } else {
-                    current_index = index_stack[index_stack_cursor--]; // 적재된 왼쪽 노드의 인덱스 정보를 꺼냄
-
-                    completion(this->_tree[current_index]);
-
-                    current_index = this->get_child_index(current_index, false); // 오른쪽 자식 인덱스 이동
-                }
-            }
-
-            delete[] index_stack;
-        }
-        void traversal_postorder(size_t root, std::function<void(t& value)> const& completion) const override {
-            auto visited_stack(new bool[this->_size]); // 방문 사실을 기록하는 스택
-            auto index_stack(new size_t[this->_size]);
-            long stack_cursor(0);
-            visited_stack[stack_cursor] = false;
-            index_stack[stack_cursor] = 0;
-
-            while (stack_cursor >= 0) {
-                auto visited(visited_stack[stack_cursor]);
-                auto index(index_stack[stack_cursor]);
-                --stack_cursor;
-
-                if (index >= this->_size || this->_tree[index] == t()) { // 인덱스가 최대 범위를 벗어났거나, 값이 유효하지 않은 경우
-                    continue;
-                }
-
-                if (visited) {
-                    completion(this->_tree[index]);
-
-                } else {
-
-                }
-            }
-
-            delete[] visited_stack;
-            delete[] index_stack;
-        }
-        void insert(t const& value) override {
-            if (this->_tree[0] == t()) { // 루트 인덱스의 값이 초기값인 경우
-                this->_tree[0] = value;
-
-                return;
-            }
-
-            size_t index(0);
-
-            while (true) {
-                if (this->_tree[index] == value) { // 중복된 값이 존재하는 경우 삽입을 중단
-                    break;
-                }
-
-                auto child_index(this->get_child_index(index, this->_tree[index] > value));
-
-                if (child_index >= this->_size) { // 자식 인덱스가 트리의 크기를 초과할 경우
-                    this->resize(child_index*2); // level 1 만큼 확장
-                }
-
-                if (this->_tree[child_index] == t()) {
-                    this->_tree[child_index] = value;
-                    break;
-                }
-
-                index = child_index;
-            }
-        }
-        void remove(t const& value) override {
-
-        }
+        explicit array_based_bst(size_t size = 14); // level 3 까지는 기본적으로 할당
+        ~array_based_bst();
+        void search(t const& value, std::function<void(size_t parent, size_t current, bool is_left)> const& completion) const override;
+        size_t search_edge(size_t root, bool is_min) const override;
+        void traversal_preorder(size_t root, std::function<void(t& value)> const& completion) const override;
+        void traversal_inorder(size_t root, std::function<void(t& value)> const& completion) const override;
+        void traversal_postorder(size_t root, std::function<void(t& value)> const& completion) const override;
+        void insert(t const& value) override;
+        void remove(t const& value) override;
     };
 
     template <typename t>
--- src/binary_search_tree.cpp
+++ src/binary_search_tree.cpp
@@ -5,6 +5,224 @@
 #include <binary_search_tree.h>
 
 namespace tk {
+    template <typename t>
+    array_based_bst<t>::array_based_bst(size_t size) :
+     _size(size),
+     _tree(new t[size]) {
+         std::fill(this->_tree, this->_tree + this->_size, t());
+    }
+
+    template <typename t>
+    array_based_bst<t>::~array_based_bst() {
+        delete[] this->_tree;
+    }
+
+    template <typename t>
+    size_t array_based_bst<t>::get_parent_index(size_t index) const {
+        return (index - (index % 2 == 1 ? 1 : 2)) / 2;
+    }
+
+    template <typename t>
+    size_t array_based_bst<t>::get_child_index(size_t index, bool is_left) const {
+        return 2 * index + (is_left ? 1 : 2);
+    }
+
+    template <typename t>
+    bool array_based_bst<t>::is_exist(size_t index) const {
+        return index < this->_size && this->_tree[index] != t();
+    }
+
+    template <typename t>
+    void array_based_bst<t>::resize(size_t new_size) {
+        if (this->_tree == nullptr) {
+            return;
+        }
+
+        auto new_tree(new t[new_size]);
+        auto end(std::copy(this->_tree, this->_tree + (new_size >= this->_size ? this->_size : new_size), new_tree));
+        std::fill(end, new_tree + new_size, t());
+
+        delete[] this->_tree;
+
+        this->_tree = new_tree;
+        this->_size = new_size;
+    }
+
+    template <typename t>
+    void array_based_bst<t>::search(t const& value, std::function<void(size_t parent, size_t current, bool is_left)> const& completion) const {
+        if (this->_tree[0] == t()) {
+            completion(0, 0, true);
+
+            return;
+        }
+
+        size_t parent_index(0);
+        size_t current_index(0);
+        bool is_left(false);
+
+        while (current_index < this->_size) { // 트리의 크기를 넘지 않을 때까지 순회
+            if (this->_tree[current_index] == value) { // 값에 해당하는 노드 인덱스에 도달한 경우
+                break;
+            }
+
+            parent_index = current_index;
+            is_left = this->_tree[parent_index] > value; // 값이 순회 도중의 노드 인덱스의 값보다 작은 경우 왼쪽, 큰 경우 오른쪽으로 설정
+            current_index = this->get_child_index(parent_index, is_left);
+        }
+
+        completion(parent_index, current_index, is_left);
+    }
+
+    template <typename t>
+    size_t array_based_bst<t>::search_edge(size_t root, bool is_min) const {
+        if (root >= this->_size) {
+            return 0;
+        }
+
+        auto current_index(root);
+
+        do {
+            current_index = this->get_child_index(current_index, is_min);
+        } while (current_index < this->_size);
+
+        return current_index;
+    }
+
+    template <typename t>
+    void array_based_bst<t>::traversal_preorder(size_t root, std::function<void(t& value)> const& completion) const {
+        auto index_stack(new size_t[this->_size]);
+        long index_stack_cursor(0);
+        index_stack[index_stack_cursor] = 0;
+
+        while (index_stack_cursor >= 0) {
+            auto current_index(index_stack[index_stack_cursor--]);
+
+            if (current_index >= this->_size || this->_tree[current_index] == t()) {
+                continue;
+            }
+
+            completion(this->_tree[current_index]);
+
+            auto right_child_index(this->get_child_index(current_index, false));
+            auto left_child_index(this->get_child_index(current_index, true));
+
+            if (right_child_index < this->_size) {
+                index_stack[++index_stack_cursor] = right_child_index;
+            }
+
+            if (left_child_index < this->_size) {
+                index_stack[++index_stack_cursor] = left_child_index;
+            }
+        }
+
+        delete[] index_stack;
+    }
+
+    template <typename t>
+    void array_based_bst<t>::traversal_inorder(size_t root, std::function<void(t& value)> const& completion) const {
+        auto index_stack(new size_t[this->_size]);
+        long index_stack_cursor(-1);
+        size_t current_index(0);
+
+        while (index_stack_cursor >= 0 || current_index < this->_size) {
+            if (current_index < this->_size && this->_tree[current_index] != t()) { // 현재 인덱스가 가르키고 있는 대상 유효할 경우(우선적으로 왼쪽 노드로 이동하기 위한 조건)
+                index_stack[++index_stack_cursor] = current_index;
+                current_index = this->get_child_index(current_index, true); // 왼쪽 자식의 인덱스 이동
+
+            } else {
+                current_index = index_stack[index_stack_cursor--]; // 적재된 왼쪽 노드의 인덱스 정보를 꺼냄
+
+                completion(this->_tree[current_index]);
+
+                current_index = this->get_child_index(current_index, false); // 오른쪽 자식 인덱스 이동
+            }
+        }
+
+        delete[] index_stack;
+    }
+
+    template <typename t>
+    void array_based_bst<t>::traversal_postorder(size_t root, std::function<void(t& value)> const& completion) const {
+        auto visited_stack(new bool[this->_size]); // 방문 사실을 기록하는 스택
+        auto index_stack(new size_t[this->_size]);
+        long stack_cursor(0);
+        visited_stack[stack_cursor] = false;
+        index_stack[stack_cursor] = 0;
+
+        while (stack_cursor >= 0) {
+            auto is_visited(visited_stack[stack_cursor]);
+            auto current_index(index_stack[stack_cursor]);
+            --stack_cursor;
+
+            if (current_index >= this->_size || this->_tree[current_index] == t()) { // 인덱스가 최대 범위를 벗어났거나, 값이 유효하지 않은 경우
+                continue;
+            }
+
+            if (is_visited) { // 이미 방문한 노드인 경우
+                completion(this->_tree[current_index]); // 왼쪽 및 오른쪽 자식 노드를 모두 방문한 부모 노드이므로 해당 인덱스를 방문
+
+            } else {
+                index_stack[++stack_cursor] = current_index; // 아직 모든 자식 노드에 대해 방문하지 않은 노드이므로 다시 적재
+                auto right_child_index(this->get_child_index(current_index, false));
+                auto left_child_index(this->get_child_index(current_index, true));
+
+                if (right_child_index < this->_size) {
+                    index_stack[++stack_cursor] = right_child_index;
+                    visited_stack[stack_cursor] = false; // 적재된 노드의 인덱스의 자식이 아직 방문되지 않았음을 표시
+                }
+
+                if (left_child_index < this->_size) {
+                    index_stack[++stack_cursor] = left_child_index;
+                    visited_stack[stack_cursor] = false;
+                }
+            }
+        }
+
+        delete[] visited_stack;
+        delete[] index_stack;
+    }
+
+    template <typename t>
+    void array_based_bst<t>::insert(t const& value) {
+        this->search(value, [this, &value](size_t parent, size_t current, bool is_left) {
+            if (current == 0) { // 루트 노드의 인덱스 값이 초기값인 경우
+                this->_tree[current] = value;
+
+                return;
+            }
+
+            if (current >= this->_size) { // 새로운 노드 인덱스를 가르키는 경우
+                this->resize(current * 2); // level 1의 크기만큼 만큼 확장
+            }
+
+            if (this->_tree[current] == t()) {
+                this->_tree[current] = value;
+            }
+        });
+    }
+
+    template <typename t>
+    void array_based_bst<t>::remove(t const& value) {
+        this->search(value, [this, &value](size_t parent, size_t current, bool is_left) {
+            if (value == this->_tree[current]) { // 트리에서 해당하는 값의 노드 인덱스를 찾아낸 경우
+                auto left_child_index(this->get_child_index(current, true));
+                auto right_child_index(this->get_child_index(current, false));
+
+                if ((left_child_index < this->_size && this->_tree[left_child_index] != t())
+                ^ (right_child_index < this->_size && this->_tree[right_child_index] != t())) {
+
+                }
+
+                if ((left_child_index >= this->_size || this->_tree[left_child_index] == t())
+                ^ (right_child_index >= this->_size || this->_tree[right_child_index] == t())) { // 한 개의 자식만 있는 노드 인덱스인 경우
+
+                }
+//                if ()
+            }
+        });
+    }
+
+
 //    template <typename t>
 //    binary_search_tree<t>::binary_search_tree():
 //    _root(nullptr),
